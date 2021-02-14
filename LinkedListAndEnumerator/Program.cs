14eea97fa92d8e26ecb70a0d85737dc136f24b65
mgalarowicz <rezi610@gmail.com>
2021-02-14 15:33:25 +0100

    Initial commit

---
+++ LinkedListAndEnumerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedListAndEnumerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
